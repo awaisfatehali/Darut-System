4aca5064a911b69e3264bde78501349de9e9cfdd
awaisfatehali <awaisfatehali@gmail.com>
2026-06-04 23:38:59 +0500

    cookies issue resloved

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,10 +1,11 @@
 const sendToken = (user, ststusCode, res) => {
   const token = user.getJwtToken();
 
-  // options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   };
 
   res.status(ststusCode).cookie("token", token, options).json({
