74c72f7e20dee862f0bb8a644630b19f215245cb
awaisfatehali <awaisfatehali@gmail.com>
2026-06-04 23:25:58 +0500

    updated cors policy to accomodate Front end

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://darut-system.vercel.app",
     credentials: true,
   })
 );
